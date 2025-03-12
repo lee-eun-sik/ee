0f716d76fa19f761d850a3d9d5aa009d01e2f8c9
dldmstlr20419@naver.com <dldms@DESKTOP-O794MR4>
2025-03-12 22:09:10 +0900

    신규 파일

---
+++ EX/src/main/java/unit01/AdditionServlet03.java
@@ -0,0 +1,39 @@
+package unit01;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class AdditionServlet03
+ */
+@WebServlet("/AdditionServlet03")
+public class AdditionServlet03 extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int num1 = 20;
+		int num2 = 10;
+		int add = num1 + num2;
+		
+		request.setAttribute("num1", num1);
+		request.setAttribute("num2", num2);
+		request.setAttribute("add", add);
+		
+		RequestDispatcher dispatcher= request.getRequestDispatcher("addition03.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
