b1d947efaf1024c6d9155f53bce8b86dfef6c98d
dldmstlr20419@naver.com <dldms@DESKTOP-O794MR4>
2025-03-13 00:43:34 +0900

    00

---
+++ EX/src/main/java/unit03/HelloServlet03.java
@@ -0,0 +1,48 @@
+package unit03;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloServlet03
+ */
+@WebServlet("/HelloServlet03")
+public class HelloServlet03 extends HttpServlet {
+	
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet03() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print("<html><body><h1>");
+		out.print("Hello Servlet");
+		out.print(" 헬로우 서블릿");
+		out.print("</h1></body></html>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
